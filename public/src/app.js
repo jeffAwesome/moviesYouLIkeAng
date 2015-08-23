4b7e148d0c24fcbed4c14c106c36a04ad093cef6
Jeff Richardson <jeff@masterpieceinteractive.com>
2015-08-23 11:53:53 -0500

    basic get request working with omdb

--- public/src/app.js
+++ public/src/app.js
@@ -1,7 +1,6 @@
 (function() {
   var app = angular.module("MoviesYouLike", ['ui.router']);
   app.constant("movieConfig", {
-    apiKey: "8e7c67585fa149a56537b63fe68776",
-    api: "http://api.themoviedb.org/3"
+    search: 'http://www.omdbapi.com/?s='
   });
 })();
--- public/src/common/Factory/movieFactory.js
+++ public/src/common/Factory/movieFactory.js
@@ -1,10 +1,24 @@
 (function() {
   
   var app = angular.module('MoviesYouLike');
-  app.factory('movieFactory', [function() {
+  app.factory('movieFactory', ['$http', 'movieConfig', function($http, movieConfig) {
     var movie = {};
-    movie.title = 'The Matrix';
-    return movie;
+    if (movie.title === "") {
+      movie.title = 'The Matrix';
+    }
+
+   movie.searchResults = [];
+    
+      
+    movie.searchMovie = function() {
+      $http.get(movieConfig.search+movie.title+'&type=movie&r=json')
+      .success(function(response){
+       movie.searchResults = response.Search;
+        console.log(movie.searchResults);
+      });
+    };
+    
+    return movie
   }]);
   
 })();
